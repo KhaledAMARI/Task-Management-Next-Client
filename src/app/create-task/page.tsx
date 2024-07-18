56fe0cd1f94dc463402174e00cacc43c050bb09e
Khaled AMARI <kha3d.amari@gmail.com>
2024-07-18 01:05:29 +0100

    - WIP: Vreate task form.

--- src/app/create-task/page.tsx
+++ src/app/create-task/page.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+"use client"
+import React, { ChangeEvent, FormEventHandler, useState } from 'react'
 
 const CreateTask = () => {
+  const initialTask = {
+    title: '',
+    description: '',
+    status: ''
+  };
+  
+  const [newTask, setNewTask] = useState(initialTask)
+  console.log("🚀 ~ CreateTask ~ newTask:", newTask)
+  const handleSubmit:FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log("🚀 ~ handleSubmit ~ e:", e)
+  }
+  const handleReset:FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log("🚀 ~ handleReset ~ e:", e)
+  }
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log("🚀 ~ handleInput ~ e:", e)
+    setNewTask(prevState => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+
   return (
-    <div>CreateTask</div>
+    <div className='flex-col justify-center items-center'>
+      <h2>Create NEw Task</h2>
+      <form className='border rounded-md bg-slate-300 flex-col justify-center items-center p-2 text-Black h-full w-full' onSubmit={handleSubmit} onReset={handleReset}>
+        <div className='flex-col'>
+          <label className='' htmlFor='title'>Title</label>
+          <input className='' type='text' id='title' name='title' value={newTask.title} onChange={handleInput} required />
+        </div>
+        <div>
+          <label htmlFor='description'>Title</label>
+          <input type='text' id='description' name='description' value={newTask.description} onChange={handleInput} required />
+        </div>
+        <div>
+          <label htmlFor='status'>Status</label>
+          <input type='text' id='status' name='status' value={newTask.status} onChange={handleInput} required />
+        </div>
+        <div>
+          <button type='submit'>Submit</button>
+          <button type='reset'>Reset</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client"
-import { useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useRouter } from 'next/navigation';
 import Board from "./components/Board";
 import { getTasks } from "./services/tasks";
 import {useTaskStore} from "./store";
 
 export default function Home() {
+  const router = useRouter();
+
   const setTasks = useTaskStore((state: any) => state.setTasks);
   const tasks = useTaskStore((state: any) => state.tasks);
+  const [taskType, setTaskType] = useState('');
   
   useEffect(() => {
     if (tasks.data.length === 0) {
@@ -14,11 +18,22 @@ export default function Home() {
     }
   }, []);
 
+  const handleTaskTypeSelection = (e: ChangeEvent<HTMLSelectElement>) => {
+    setTaskType(e.target.value);
+    router.push('/create-task');
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1>Task Management Board</h1>
       </div>
+      <select value={taskType} onChange={handleTaskTypeSelection} className='p-3 bg-white text-black'>
+        <option value="" disabled>Add New Task</option>
+        <option value="pending">Pending</option>
+        <option value="in-progress">In Progress</option>
+        <option value="done">Done</option>
+      </select>
       <Board />
     </main>
   );
